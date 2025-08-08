8327ecbcec86377a550e8777aa33f16524a55e0f
LuckedCoronet45 <lc45@lc-studios.net>
2025-08-08 20:58:31 +0900

    add kill_item.js
    When experimenting with destructive features, you often wanna kill all
    floating items in the world!

---
+++ src/bp/scripts/commands/kill_item.js
@@ -0,0 +1,40 @@
+import * as mc from "@minecraft/server";
+import { defineCommandCallback } from "../utils/define_command.js";
+import { prefixCommandDescription } from "../utils/misc.js";
+
+const callback = defineCommandCallback((origin) => {
+	const player = origin.sourceEntity;
+	if (!(player instanceof mc.Player)) {
+		return {
+			status: mc.CustomCommandStatus.Failure,
+			message: "Only a player can run this command.",
+		};
+	}
+
+	mc.system.run(() => {
+		const result = player.runCommand("kill @e[type=item]");
+
+		player.onScreenDisplay.setActionBar(
+			`Killed ${result.successCount} item entities.`,
+		);
+
+		player.playSound("random.burp", {
+			location: player.location,
+			pitch: 1.2,
+		});
+	});
+});
+
+mc.system.beforeEvents.startup.subscribe(({ customCommandRegistry }) => {
+	customCommandRegistry.registerCommand(
+		{
+			name: "n:x",
+			description: prefixCommandDescription(
+				"Kill all floating items in the world. Alias for /kill @e[type=item]",
+			),
+			permissionLevel: mc.CommandPermissionLevel.Admin,
+			cheatsRequired: true,
+		},
+		callback,
+	);
+});
--- src/bp/scripts/main.js
+++ src/bp/scripts/main.js
@@ -2,6 +2,7 @@
 // modules that contain business logic.
 
 import "./commands/clear_hotbar.js";
+import "./commands/kill_item.js";
 import "./commands/move_forward.js";
 import "./commands/nv_toggle.js";
 import "./commands/spectator_toggle.js";
