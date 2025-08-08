63c2b9cfb662bc2640a487d53463d092fe147d94
LuckedCoronet45 <lc45@lc-studios.net>
2025-08-08 09:43:16 +0900

    add nv_toggle.js
    Command for quickly toggling the night vision effect. Having to type the
    long `/effect` command is extremely annoying when you are building
    something underground.

---
+++ src/bp/scripts/commands/nv_toggle.js
@@ -0,0 +1,41 @@
+import * as mc from "@minecraft/server";
+import { defineCommandCallback } from "../shared.js";
+
+const callback = defineCommandCallback((origin) => {
+	const player = origin.sourceEntity;
+	if (!(player instanceof mc.Player)) {
+		return {
+			status: mc.CustomCommandStatus.Failure,
+			message: "Only a player can run this command.",
+		};
+	}
+
+	mc.system.run(() => {
+		const alreadyHasEffect = player.getEffect("night_vision") !== undefined;
+
+		if (alreadyHasEffect) {
+			player.removeEffect("night_vision");
+			player.onScreenDisplay.setActionBar("Removed the night vision effect.");
+			return;
+		}
+
+		player.addEffect("night_vision", 20000000, {
+			amplifier: 0,
+			showParticles: false,
+		});
+
+		player.onScreenDisplay.setActionBar("Added the night vision effect.");
+	});
+});
+
+mc.system.beforeEvents.startup.subscribe(({ customCommandRegistry }) => {
+	customCommandRegistry.registerCommand(
+		{
+			name: "n:v",
+			description: "Toggles the night vision effect.",
+			permissionLevel: mc.CommandPermissionLevel.Admin,
+			cheatsRequired: true,
+		},
+		callback,
+	);
+});
--- src/bp/scripts/main.js
+++ src/bp/scripts/main.js
@@ -1,2 +1,4 @@
 // This is the entry point script, and its sole purpose is to import other
 // modules that contain business logic.
+
+import "./commands/nv_toggle.js";
