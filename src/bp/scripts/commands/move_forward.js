ee45c83da355721dd53d53add8e13eadc6d02e51
LuckedCoronet45 <lc45@lc-studios.net>
2025-08-08 14:21:04 +0900

    add move_forward.js
    Sometimes you wanna move forward fast!

---
+++ src/bp/scripts/commands/move_forward.js
@@ -0,0 +1,67 @@
+import * as mc from "@minecraft/server";
+import { defineCommandCallback } from "../utils/define_command.js";
+import { prefixCommandDescription } from "../utils/misc.js";
+import { Vec3 } from "../utils/vec3.js";
+
+/**
+ * @param {mc.Player} player
+ * @param {mc.Vector3} location
+ */
+const teleport = (player, location) => {
+	player.teleport(location);
+	player.onScreenDisplay.setActionBar("Moved forward");
+};
+
+const callback = defineCommandCallback((origin) => {
+	const player = origin.sourceEntity;
+	if (!(player instanceof mc.Player)) {
+		return {
+			status: mc.CustomCommandStatus.Failure,
+			message: "Only a player can run this command.",
+		};
+	}
+
+	mc.system.run(() => {
+		const maxDistance = 100;
+
+		const raycastHit = player.getBlockFromViewDirection();
+
+		if (
+			!raycastHit ||
+			Vec3.distance(raycastHit.block.center(), player.getHeadLocation()) > 100
+		) {
+			const tpLocation = new Vec3(Vec3.forward)
+				.scale(maxDistance)
+				.changeDir(player.getViewDirection())
+				.add(player.getHeadLocation()).vec;
+
+			teleport(player, tpLocation);
+			return;
+		}
+
+		let tpLocation = new Vec3(player.getHeadLocation())
+			.sub(raycastHit.block.center())
+			.normalize()
+			.add(raycastHit.block.center()).vec;
+
+		if (raycastHit.face === mc.Direction.Down) {
+			tpLocation = Vec3.sub(tpLocation, { x: 0, y: 1.5, z: 0 });
+		}
+
+		teleport(player, tpLocation);
+	});
+});
+
+mc.system.beforeEvents.startup.subscribe(({ customCommandRegistry }) => {
+	customCommandRegistry.registerCommand(
+		{
+			name: "n:x",
+			description: prefixCommandDescription(
+				"Moves forward 100 blocks. Stops when the ray hits a block.",
+			),
+			permissionLevel: mc.CommandPermissionLevel.Admin,
+			cheatsRequired: true,
+		},
+		callback,
+	);
+});
--- src/bp/scripts/main.js
+++ src/bp/scripts/main.js
@@ -2,5 +2,6 @@
 // modules that contain business logic.
 
 import "./commands/clear_hotbar.js";
+import "./commands/move_forward.js";
 import "./commands/nv_toggle.js";
 import "./commands/spectator_toggle.js";
