a687f7feff1848010a042980e40fc104cc54ab8c
LuckedCoronet45 <lc45@lc-studios.net>
2025-08-09 12:37:31 +0900

    update scripts/utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,7 @@
 import path from "node:path";
 import { homedir } from "node:os";
+import fs from "node:fs";
+import archiver from "archiver";
 
 /** @returns {{ behaviorPacks: string, resourcePacks: string }} */
 export function getDevPackDirs() {
@@ -18,3 +20,22 @@ export function getDevPackDirs() {
 		resourcePacks,
 	};
 }
+
+/**
+ * @param {string} sourceDir
+ * @param {string} outputPath
+ * @returns {Promise<void>}
+ */
+export const zipDirectory = (sourceDir, outputPath) => {
+	return new Promise((resolve, reject) => {
+		const output = fs.createWriteStream(outputPath);
+		const archive = archiver("zip");
+
+		output.on("close", resolve);
+		archive.on("error", reject);
+
+		archive.pipe(output);
+		archive.directory(sourceDir, false);
+		archive.finalize();
+	});
+};
