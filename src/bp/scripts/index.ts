6fc6da9fa73bcdcc6e7a884e50bc6b89e9cd436b
LuckedCoronet <lc45@lc-studios.net>
2025-12-01 13:31:46 +0900

    refactor: update imports in index.ts

--- src/bp/scripts/index.ts
+++ src/bp/scripts/index.ts
@@ -1,11 +1,11 @@
 // This is the entry point script, and its sole purpose is to import other
 // modules that contain business logic.
 
-import "./commands/clear_effects.js";
-import "./commands/clear_hotbar.js";
-import "./commands/help.js";
-import "./commands/kill_item.js";
-import "./commands/move_forward.js";
-import "./commands/nv_toggle.js";
-import "./commands/smart_gamemode.js";
-import "./commands/user_custom_commands.js";
+import "./commands/clear_effects";
+import "./commands/clear_hotbar";
+import "./commands/help";
+import "./commands/kill_item";
+import "./commands/move_forward";
+import "./commands/nv_toggle";
+import "./commands/smart_gamemode";
+import "./commands/user_custom_commands";
